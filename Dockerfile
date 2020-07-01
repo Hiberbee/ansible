e01ac16394eb8ac182d07f7bebd7817e56cf90c8
Vlad Volkov <vlad@hiberbee.com>
2020-07-01 07:36:12 +0300

    Update role versions & docker configuration

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,5 @@
 FROM ubuntu:20.04
-RUN apt-get update --yes \
- && apt-get full-upgrade --yes \
- && apt-get install --yes \
-      python3-pip \
- && pip3 install --upgrade --prefer-binary ansible
+COPY provision.sh /usr/local/bin/provision
 WORKDIR /etc/ansible
-COPY . .
-RUN ansible-playbook playbook.yml \
- && apt-get purge python3-wheel python3-setuptools --yes \
- && apt-get autoremove --yes \
- && apt-get autoclean --yes \
- && apt-get clean \
- && py3clean /usr \
- && rm -rf ~/.cache/pip/* /var/cache/apt/*
-ENTRYPOINT ["fish", "-C"]
+RUN /usr/local/bin/provision \
+ && ansible-playbook playbook.yml
---
+++ provision.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env sh
+
+hostUser=${HOST_USER}
+ansibleDirectory=/etc/ansible
+
+# shellcheck disable=SC2039
+if [ -f /etc/os-release ]; then
+  . "/etc/os-release"
+  distribution=$ID
+else
+  echo "ERROR: I need the file /etc/os-release to determine what my distribution is..."
+  exit
+fi
+
+echo "Upgrading system packages"
+case ${distribution} in
+alpine)
+  apk add --no-cache --update ansible wget
+  rm -rf /var/cache/apk/*
+  find /usr/lib/python3.8/site-packages | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
+  ;;
+centos | fedora | redhat)
+  yum update -y
+  yum install -y ansible
+  find /usr/lib/python3/site-packages | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
+  ;;
+arch)
+  pacman-key --init
+  pacman-key --refresh-keys
+  yes | pacman -Syu ansible
+  ;;
+ubuntu | debian)
+  apt-get update
+  apt-get install --yes python3-setuptools python3-distutils python3-wheel wget
+  wget -qO- https://bootstrap.pypa.io/get-pip.py | python3
+  pip3 install --upgrade --prefer-binary ansible
+  apt-get purge snapd pulseaudio-utils fonts-dejavu-core git python3-setuptools python3-wheel --yes
+  apt-get autoremove --yes
+  apt-get autoclean --yes
+  apt-get clean
+  py3clean /usr
+  rm -rf ~/.cache/pip/* /var/cache/apt/*
+  ;;
+*)
+  echo "No actions provided for $distribution"
+  ;;
+esac
+
+if [ -d "/mnt/c/Users/${hostUser}/.ssh" ]; then
+  echo "Linking ssh keys"
+  cp -rf "/mnt/c/Users/$hostUser/.ssh" "$HOME/.ssh"
+fi
+
+[ -d ${ansibleDirectory} ] || mkdir ${ansibleDirectory}
+cd ${ansibleDirectory} || exit
+
+echo "Downloading Ansible roles"
+wget "https://github.com/Hiberbee/ansible/archive/master.tar.gz" -qO- | tar xvz --strip=1
+
+if [ -f "requirements.yml" ]; then
+  echo "Found Galaxy requirements file, installing roles..."
+  ansible-galaxy install -r requirements.yml
+fi
+
+if [ -f "playbook.yml" ]; then
+  echo "Found Ansible playbook, running tasks..."
+  ansible-galaxy install -r requirements.yml
+fi
