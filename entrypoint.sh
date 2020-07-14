31a07101a111bc034441cec9806c2f114edc1307
Vlad Volkov <vlad@hiberbee.com>
2020-07-14 03:27:25 +0300

    Updated docker installation

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ echo "Upgrading system packages"
 case ${distribution} in
 alpine)
   apk add --no-cache --update py3-pip wget python3-dev gcc musl-dev libffi-dev openssl-dev build-base
-  pip3 install --upgrade ansible ansible-lint molecule
+  pip3 install --upgrade ansible ansible-lint
   rm -rf /var/cache/apk/*
   apk del python3-dev gcc musl-dev libffi-dev openssl-dev build-base
   find /usr/lib/python3.8/site-packages | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
@@ -24,7 +24,7 @@ alpine)
 centos | fedora | redhat)
   dnf update
   dnf install --yes python3 python3-pip wget
-  pip3 install --upgrade ansible ansible-lint molecule
+  pip3 install --upgrade ansible ansible-lint
   find /usr/lib/python3/site-packages | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
   ;;
 arch)
@@ -36,7 +36,7 @@ ubuntu | debian)
   apt-get update
   apt-get install --yes python3-setuptools python3-distutils python3-wheel wget
   wget -qO- https://bootstrap.pypa.io/get-pip.py | python3
-  pip3 install --upgrade ansible ansible-lint molecule
+  pip3 install --upgrade ansible ansible-lint
   apt-get purge snapd pulseaudio-utils fonts-dejavu-core git python3-setuptools python3-wheel --yes
   apt-get autoremove --yes
   apt-get autoclean --yes
